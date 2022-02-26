560ad6429002a2492cd4259b36cfcb5fbc4c0cf4
andrius-siup <andrius.siupinys@gmail.com>
2022-02-26 13:52:24 +0000

    Made active hamburger button - open and close mobile navigation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation active
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
