2cc6db05e76dfb2d158d9d571435e790c825865c
andrius-siup <andrius.siupinys@gmail.com>
2022-02-26 15:22:25 +0000

    Added smooth scrolling animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+("use-strict");
+
 // Set current year into copyrights
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -9,4 +11,28 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      // console.log(href);
+      const sectionEl = document.querySelector(href);
+      // console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 });
